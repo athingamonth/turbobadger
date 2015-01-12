d568e977afd352da7c066dfc65e98c8df090246d
Andrew Thorpe <pyroka@gmail.com>
2015-01-12 22:21:34 +0000

    Improved the colouring of certain words

--- Demo/demo01/CodeEditField/CodeEditField.cpp
+++ Demo/demo01/CodeEditField/CodeEditField.cpp
@@ -1,6 +1,7 @@
 #include "CodeEditField.h"
 
 #include "tb_widgets_reader.h"
+#include <ctype.h>
 
 using namespace tb;
 TB_WIDGET_FACTORY(CodeEditField, TBValue::TYPE_STRING, WIDGET_Z_TOP) {}
@@ -12,9 +13,27 @@ void CodeEditField::OnInflate(const INFLATE_INFO &info)
 void CodeEditField::DrawString(int32 x, int32 y, TBFontFace *font, const TBColor &color, const char *str, int32 len)
 {
 	TBColor finalColor(color);
-	if (strncmp(str, "code", len) == 0)
-	{
-		finalColor = TBColor(255, 0, 208);
-	}
+	StringHasColorOverride(str, len, finalColor);
 	TBEditField::DrawString(x, y, font, finalColor, str, len);
+}
+
+bool CodeEditField::StringHasColorOverride(const char* str, int32 len, TBColor& colour)
+{
+	char* matchAgainst = "code";
+	int32 matchLen = (int32)strlen(matchAgainst);
+	if (matchLen != len)
+	{
+		return false;
+	}
+
+	for (int32 i = 0; i < len; i++)
+	{
+		if (toupper(matchAgainst[i]) != toupper(str[i]))
+		{
+			return false;
+		}
+	}
+
+	colour = TBColor(255, 0, 208);
+	return true;
 }
--- Demo/demo01/CodeEditField/CodeEditField.h
+++ Demo/demo01/CodeEditField/CodeEditField.h
@@ -2,10 +2,20 @@
 
 #include "tb_editfield.h"
 
+#include <vector>
+
 class CodeEditField : public tb::TBEditField
 {
+	enum SpecialStringTypes
+	{
+		Keyword,
+		Variable
+	};
+
 	virtual void OnInflate(const tb::INFLATE_INFO &info);
 
 private:
 	virtual void DrawString(tb::int32 x, tb::int32 y, tb::TBFontFace *font, const tb::TBColor &color, const char *str, tb::int32 len);
+
+	bool StringHasColorOverride(const char* str, tb::int32 len, tb::TBColor& colour);
 };
